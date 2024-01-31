af14acc4e64735bfa5c0b1b1a603f8a19e5549eb
tenecota junior <jtenecotac2@uteq.edu.ec>
2024-01-31 10:22:40 -0500

    ACTUALIZADO

--- app/src/main/java/com/example/google_maps/MainActivity.java
+++ app/src/main/java/com/example/google_maps/MainActivity.java
@@ -4,19 +4,32 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class MainActivity extends FragmentActivity
+        implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     GoogleMap mapa;
+    int contador;
+    ArrayList<LatLng> puntos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +37,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
+        contador=0;
+        puntos =new ArrayList<LatLng>();
     }
 
     @Override
@@ -33,23 +47,75 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         mapa.getUiSettings().setZoomControlsEnabled(true);
-        //CameraUpdate camUpd1 = CameraUpdateFactory
-          //      .newLatLngZoom(new LatLng(49.26892181964082, -123.23316638271675), 15);
-        //mapa.moveCamera(camUpd1);
-        LatLng madrid = new LatLng(40.68922431127387, -74.04433046256183);
-
-        CameraPosition camPos = new CameraPosition.Builder()
-                .target(madrid)
-                .zoom(20)
-                .bearing(50)      //noreste arriba
-                .tilt(10)         //punto de vista de la cámara 70 grados
-                .build();
-
-        CameraUpdate camUpd3 =
-                CameraUpdateFactory.newCameraPosition(camPos);
-
-        mapa.animateCamera(camUpd3);
+        CameraUpdate camUpd1 = CameraUpdateFactory
+               .newLatLngZoom(new LatLng(-1.0127272376147443, -79.46956271012085), 15);
+        mapa.moveCamera(camUpd1);
+        mapa.setOnMapClickListener(this);
 
 
     }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+       // Projection proj = mapa.getProjection();
+        //Point coord = proj.toScreenLocation(latLng);
+
+        TextView lbtLat = findViewById(R.id.txt_latit);
+        lbtLat.setText(String.format("%.4f", latLng.latitude));
+
+        TextView lblLong = findViewById(R.id.txt_longi);
+        lblLong.setText(String.format("%.4f",latLng.longitude));
+
+        mapa.addMarker(new MarkerOptions().position(latLng)
+                .title("Marcador"));
+
+         contador= contador + 1;
+        puntos.add(latLng);
+        if (contador==4){
+            PolylineOptions lineas = new PolylineOptions()
+                    .add(puntos.get(0))
+                    .add(puntos.get(1))
+                    .add(puntos.get(2))
+                    .add(puntos.get(3))
+                    .add(puntos.get(0));
+            lineas.width(8);
+            lineas.color(Color.RED);
+            mapa.addPolyline(lineas);
+            contador=0;
+            puntos.clear();
+        }
+
+        //Toast.makeText(
+          //          MainActivity.this,
+            //        "Click\n" +
+              //      "Lat: " + latLng.latitude + "\n" +
+                //    "Lng: " + latLng.longitude + "\n" +
+                  //  "X: " + coord.x + " - Y: " + coord.y,
+                    //Toast.LENGTH_SHORT).show();
+    }
+    public void PintarRecUTEQ(View view){
+        mapa.addMarker(new MarkerOptions().position(
+                new LatLng(-1.0126102537465256, -79.47058724772829)));
+        mapa.addMarker(new MarkerOptions().position(
+                new LatLng(-1.0132265550724981, -79.47041844959088)));
+        mapa.addMarker(new MarkerOptions().position(
+                new LatLng(-1.0133536079466559, -79.46910599665732)));
+        mapa.addMarker(new MarkerOptions().position(
+                new LatLng(-1.0129269378271246, -79.46933738286815)));
+
+
+        PolylineOptions lineas = new PolylineOptions()
+                .add(new LatLng(-1.0126102537465256, -79.47058724772829))
+                .add(new LatLng(-1.0132265550724981, -79.47041844959088))
+                .add(new LatLng(-1.0133536079466559, -79.46910599665732))
+                .add(new LatLng(-1.0129269378271246, -79.46933738286815))
+                .add(new LatLng(-1.0126102537465256, -79.47058724772829));
+
+        lineas.width(8);
+        lineas.color(Color.RED);
+
+        mapa.addPolyline(lineas);
+
+    }
+
 }
